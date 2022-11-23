dd17cfae5c59cd85cbf4ed4cddf017cddb93970b
Nassir Al-Khishman <khishmann@gmail.com>
2022-11-22 21:13:16 -0500

    feat(load testing) add load tests and server setup

---
+++ sync/tests/load/locustfile.py
@@ -0,0 +1,27 @@
+import locust
+
+from utils import get_random_string
+
+
+class CpuUser(locust.HttpUser):
+    @locust.task
+    def basic(self):
+        self.client.get("/posts/cpu/")
+
+
+class PostCreateUser(locust.HttpUser):
+    @locust.task
+    def basic(self):
+        self.client.post(
+            "/posts/io_create/",
+            data={
+                "title": f"Post {get_random_string(10)}",
+                "content": f"Content {get_random_string(100)}",
+            },
+        )
+
+
+class PostReadUser(locust.HttpUser):
+    @locust.task
+    def basic(self):
+        self.client.get("/posts/io_read/")
